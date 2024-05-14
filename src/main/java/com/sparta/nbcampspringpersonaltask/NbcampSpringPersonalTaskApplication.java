a2e5588fd3609c6e066f968e21a94ef02c96e1b0
ydh5017 <ydh5017@naver.com>
2024-05-14 14:20:15 +0900

    init commit

---
+++ src/main/java/com/sparta/nbcampspringpersonaltask/NbcampSpringPersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcampspringpersonaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampSpringPersonalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampSpringPersonalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nbcampspringpersonaltask/NbcampSpringPersonalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcampspringpersonaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampSpringPersonalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
